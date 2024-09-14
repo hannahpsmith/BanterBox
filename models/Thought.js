7a6b258e5885b3693fd0d6c909739d8ef9decbaf
hannahpsmith <166345156+hannahpsmith@users.noreply.github.com>
2024-09-14 16:34:20 -0600

    added models

---
+++ models/Thought.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose');
+const Reaction = require('./Reaction')
+
+const thoughtSchema = new Schema (
+    {
+        thoughtText: {
+          type: String,
+          required: true,
+          minlength: 1,
+          maxlength: 280
+        },
+
+        createdAt: {
+          type: Date,
+          default: Date.now,
+          get: (date) => moment(date).format('MM/DD/YYYY') 
+        },
+
+        
+        username: {
+          type: String,
+          required: true
+        },
+
+        reactions: [Reaction],
+      },
+      {
+        toJSON: { 
+          virtuals: true, 
+          getters: true,
+        }, 
+
+        id: false
+      }
+);
+
+//Gets the number of reactions for a given thought
+thoughtSchema
+    .virtual('reactionCount')
+    .get(function () {
+        return this.reactions.length;
+    });
+
+const Thought = model('thought', thoughtSchema);
+    
+module.exports = Thought;
