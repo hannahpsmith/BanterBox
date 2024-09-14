2ec858ba5a757013fb6399b74520cb2ab781a5a7
hannahpsmith <166345156+hannahpsmith@users.noreply.github.com>
2024-09-14 14:57:56 -0600

    created user model

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'Please enter a valid email address'],
+        },
+
+        thoughts: [
+            { type: Schema.Types.ObjectId, ref: 'thought' }
+        ],
+
+        friends: [
+            { type: Schema.Types.ObjectId, ref: 'user' }
+        ],
+    },
+        {
+        toJSON: { 
+            virtuals: true,
+        },
+
+        id: false,
+    }
+);
+
+//Virtual field to get length of friends and return friend count
+userSchema
+    .virtual('friendCount')
+    .get(function () {
+        return this.friends.length;
+    });
+    
+//Create and export user model
+const User = model('user', userSchema);
+
+module.exports = User;
---
+++ models/index.js
@@ -0,0 +1,7 @@
+// Import models
+const User = require('./User');
+const Thought = require('./Thought');
+const Reaction = require('./Reaction');
+
+// Exporting models
+module.exports = { User, Thought, Reaction };
